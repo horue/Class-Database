43c22f19dec677ffa147fbc6a584db9b4ba42abb
Jorge <jorgejapao00@gmail.com>
2024-04-04 18:40:10 -0300

    Function to show text to add class member created

--- menus.py
+++ menus.py
@@ -7,6 +7,7 @@ import os
 connection=sqlite3.connect("class.db")
 
 cursor=connection.cursor()
+
 #cursor.execute("CREATE TABLE Alunos (Nome TEXT, Curso TEXT, Matrícula INTERGER)")
 
 
@@ -15,31 +16,33 @@ cursor=connection.cursor()
 def princiapl_menu():
     option=input("> ")
     if option.lower() ==("buscar"):
-        sys.exit #start_game()
-    elif option.lower() == ("Adicionar Aluno"):
+        sys.exit
+    elif option.lower() == ("adicionar", "adicionar aluno"):
         add_aluno()
     elif option.lower() ==  ('ajuda'):
         ajuda()
     elif option.lower() == ("sair"):
         sys.exit
-    while option.lower() not in ['buscar', 'adicionar aluno', 'ajuda', 'sair']:
+    while option.lower() not in ['buscar', 'adicionar', 'adicionar aluno', 'ajuda', 'sair']:
         print("Por favor, entre um comando válido.")
         option=input("> ")
         return
 
 
 def add_aluno():
-    print("Entre as informações do Aluno a ser adicionado:")
-    nome=input("Nome do aluno: ")
-    curso=input("Curso do aluno: ")
-    mat=input("Matrícula do aluno: ")
-    cursor.execute(f"INSERT INTO Alunos VALUES ({nome, curso, mat})")
+    while True:
+        nome=input("Nome do aluno: ")
+        curso=input("Curso do aluno: ")
+        mat=input("Matrícula do aluno: ")
+        cursor.execute(f"INSERT INTO Alunos VALUES ({nome, curso, mat})")
+        return
 
 
 def ajuda_menu():
     option=input("> ")
     if option.lower() ==  ('mais'):
         wb.open('https://github.com/horue')
+        principal()
     if option.lower() == ('voltar'):
         principal()
     while option.lower() not in ['mais', 'voltar']:
@@ -48,15 +51,13 @@ def ajuda_menu():
         return
 
 
-
-
-
     
 def principal():
     os.system('cls')
-    print("#" * 30)
+    print("#" * 35)
+    print('# Class Database - Tela Inicial #')
     print('# Bem vindo à Class Database! #')
-    print("#" * 30)
+    print("#" * 35)
     print('Buscar Aluno')
     print('Adicionar Aluno')
     print('Ajuda')
@@ -65,11 +66,22 @@ def principal():
     princiapl_menu()
 
 
+def aluno():
+    print("#" * 35)
+    print('# Class Database - Adicionando Aluno #')
+    print("#" * 35)
+    print("Entre as informações do Aluno a ser adicionado:")
+    add_aluno()
+    return(principal)
+
+
+
+
 def ajuda():
     os.system('cls')
-    print("#" * 30)
+    print("#" * 35)
     print('# Class Database - Tela de Ajuda #')
-    print("#" * 30)
+    print("#" * 35)
     print('— Use comandos para acessar, alterar ou apagar dados no banco de dados.')
     print("— Caso ainda tenha alguma dúvida, use o comando 'Mais' para ser redirecionado à docmuentação do programa.")
     print("— Para voltar à tela anterior, use o comando 'Voltar'.")
